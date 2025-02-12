0eede48108950095b10c1723a330c0ea963637b8
Baczkiewicz Damien <baczkiewicz.damien@hotmail.fr>
2025-02-12 14:24:37 +0100

    feat: authentification with argon, jwt and cookies

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
 import Login from "./pages/LoginPage";
 import Register from "./pages/RegisterPage";
-import HomePage from "./pages/HomePage";
 
 export const router = createBrowserRouter([
   {
